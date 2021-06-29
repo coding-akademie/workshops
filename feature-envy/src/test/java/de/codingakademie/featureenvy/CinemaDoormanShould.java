8530350b0176109155c72ea38c040af3fb699350
Michael Mai <michael@agilesoftwaredesign.de>
2021-06-29 14:31:06 +0200

    Working on feature-envy: Improve unit test
    A minimal-viable unit test. Normally, more tests are needed for a better
    safety-net - for the purpose of this lecture unit, this single unit test
    is barely-sufficient.

--- feature-envy/src/test/java/de/codingakademie/featureenvy/CinemaDoormanShould.java
+++ feature-envy/src/test/java/de/codingakademie/featureenvy/CinemaDoormanShould.java
@@ -1,13 +1,16 @@
 package de.codingakademie.featureenvy;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
 public class CinemaDoormanShould {
     @Test
     void permit_access_to_old_people() {
-        new CinemaDoorman().main(null);
-        assertEquals(0, 1);
+        CinemaDoorman doorman = new CinemaDoorman();
+        User anton = new User(18);
+        Movie movie = new Movie("Horror of the Code");
+
+        assertFalse(doorman.isUserAllowedToVisitTheMovie(anton, movie));
     }
 }
