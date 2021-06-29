ac59e49cda7c84f548756619659bca4f087db333
Michael Mai <michael@agilesoftwaredesign.de>
2021-06-29 15:03:31 +0200

    Working on feature-envy: Added ability to Wallet to fund with more than no money

--- feature-envy/src/main/java/de/codingakademie/featureenvy/Wallet.java
+++ feature-envy/src/main/java/de/codingakademie/featureenvy/Wallet.java
@@ -3,6 +3,13 @@ package de.codingakademie.featureenvy;
 public class Wallet {
     private Money value = new Money("0.00", "EUR");
 
+    public Wallet() {
+    }
+
+    public Wallet(Money initialWalletFund) {
+        value = new Money(initialWalletFund);
+    }
+
     public int getAmount() {
         // TODO Auto-generated method stub
         return 0;
